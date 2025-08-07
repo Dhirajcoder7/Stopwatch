8c3a29d709b535cf40125686707da88f7cf247ff
Dhiraj Gupta <dhirajcoder777@gmail.com>
2025-08-07 23:13:59 +0530

    Update Stopwatch.jsx

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -27,15 +27,15 @@ const Stopwatch = () => {
   return (
     <>
       
-      <div className=' m-auto my-30 size-fit p-5 rounded-4xl bg-neutral-600 '>
+      <div className=' m-auto my-50 size-fit p-15 px-8 rounded-2xl bg-gray-800 shadow-[4px_4px_10px_black]  '>
         <h1 className='text-white text-3xl mb-4 text-center font-semibold '>STOPWATCH</h1>
-        <div className='text-white bg-neutral-400  px-7 p-5 rounded-2xl shadow-2xl text-4xl font-mono font-semibold mb-4  '>
+        <div className='text-white bg-blue-700 px-10 p-5 rounded-2xl shadow-2xl text-4xl font-mono font-semibold mb-4  '>
           {formatTime(time)}
         </div>
-        <div className='space-x-4 '>
-          <button onClick={() => setIsRunning(true)} className="transition-all duration-250 bg-green-500 hover:bg-green-700 rounded-xl text-white font-semibold px-3 py-1.5 cursor-pointer">Start</button>
-          <button onClick={() => setIsRunning(false)} className="transition-all duration-250 bg-yellow-400 hover:bg-yellow-600 rounded-xl text-white font-semibold px-3 py-1.5 cursor-pointer">Stop</button>
-          <button onClick={() => { setIsRunning(false); setTime(0); }} className="transition-all duration-250 hover:bg-red-700 bg-red-600 rounded-xl text-white font-semibold px-3 py-1.5 cursor-pointer">Reset</button>
+        <div className='space-x-4 text-center '>
+          <button onClick={() => setIsRunning(true)} className="mt-4 transition-all duration-250 bg-green-500 hover:bg-green-700 rounded-md text-white font-semibold px-3 py-1.5 cursor-pointer">Start</button>
+          <button onClick={() => setIsRunning(false)} className="transition-all duration-250 bg-yellow-400 hover:bg-yellow-600 rounded-md text-white font-semibold px-3 py-1.5 cursor-pointer">Stop</button>
+          <button onClick={() => { setIsRunning(false); setTime(0); }} className="transition-all duration-250 hover:bg-red-700 bg-red-600 rounded-md text-white font-semibold px-3 py-1.5 cursor-pointer">Reset</button>
         </div>
       </div>
     </>
